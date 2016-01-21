cb4efb96be2a8ac7c5aa4511cf34563f6d0eb95d
James Anderson <caracallarex@gmail.com>
2016-01-21 10:41:38 -0800

    work in progress

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ActiveRecord::Base
-  validates :sender_id, :messageable_id, :messageable_type, :read, presence: true
+  validates :sender_id, :messageable_id, :messageable_type, presence: true
   validates :money, numericality: { greater_than_or_equal_to: 0 }
 
   belongs_to :sender, class: User, foreign_key: "sender_id"
